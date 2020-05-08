05b812619521af688d53a7ec84ae05406051580f
charleschz <44064966+charleschz@users.noreply.github.com>
2020-05-08 17:08:50 +0800

    Add files via upload

---
+++ 程序源代码/后端代码/后台源码/src/main/java/work/code/mango/mapper/MangoNewMessageMapper.java
@@ -0,0 +1,13 @@
+package work.huangxin.mango.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import work.huangxin.mango.model.MangoNewMessage;
+a.util.List;
+
+public interface MangoNewMessageMapper extends Mapper<MangoNewMessage> {
+
+    List<MangoNewMessage> getAllNewMessage(Integer id);
+
+
+    MangoNewMessage getLastNewMessage(Integer id);
+}
